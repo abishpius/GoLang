5250cfd3bac1134393747e01ca7a47fea33af082
Abish Pius <60949830+abishpius@users.noreply.github.com>
2022-04-07 21:07:25 -0400

    concurrency and methods lessons

---
+++ SRC/methods/methods3.go
@@ -0,0 +1,24 @@
+package geometry
+
+type Triangle struct {
+	size int
+}
+
+func (t *Triangle) doubleSize() {
+	t.size *= 2
+}
+
+func (t *Triangle) SetSize(size int) {
+	t.size = size
+}
+
+func (t *Triangle) Perimeter() int {
+	t.doubleSize()
+	return t.size * 3
+}
+
+func main() {
+	t := geometry.Triangle{}
+	t.SetSize(3)
+	fmt.Println("Perimeter", t.Perimeter())
+}
